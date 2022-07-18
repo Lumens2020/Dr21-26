65cba305bd0cc1d83b491fdcdf48cce01512a1b0
“lumens” <360276548@qq.com>
2022-07-18 08:59:49 +0800

    modify

--- HexToBin/Command_hex2bin.bat
+++
@@ -1,3 +0,0 @@
-cd /d C:\ProjectData\P2126\software\V4.1\C\HexToBin\P2126_WashFloor.hex
-hex2bin -e bin P2126_WashFloor.hex
-exit
--- P2126_WashFloor_MM32SPIN27PF/P2126_WashFloor/HARDWARE/readme.txt
+++
@@ -1,2 +0,0 @@
-MCUConfigure:        单片机外设配置函数
-WashFloorFunction: 洗地机功能函数
